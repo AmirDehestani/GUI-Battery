d4017665a8806e432f3a140b4434cf47aa1f832f
AmirDehestani <amira.dehestani@gmail.com>
2024-04-12 16:48:38 -0400

    Create ros subscriber for battery voltages

--- battery/src/app/battery-status-window/battery-status-window.component.ts
+++ battery/src/app/battery-status-window/battery-status-window.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BatteryInfoComponent } from '../battery-info/battery-info.component';
+import { RosService } from '../ros.service';
 
 @Component({
   selector: 'app-battery-status-window',
@@ -8,7 +9,24 @@ import { BatteryInfoComponent } from '../battery-info/battery-info.component';
   templateUrl: './battery-status-window.component.html',
   styleUrl: './battery-status-window.component.css',
 })
-export class BatteryStatusWindowComponent {
-  leftBatteryVoltage : number = 15.2
-  rightBatteryVoltage : number = 14.5
+export class BatteryStatusWindowComponent implements OnInit {
+  leftBatteryVoltage: number;
+  rightBatteryVoltage: number;
+
+  constructor(private rosService: RosService) {
+    this.leftBatteryVoltage = 0; // Initial value
+    this.rightBatteryVoltage = 0; // Initial value
+  }
+
+  ngOnInit(): void {
+    // Subscribe to the left battery voltage topic
+    this.rosService.subscribeToLeftBatteryVoltage((data: number) => {
+      this.leftBatteryVoltage = data;
+    });
+
+    // Subscribe to the right battery voltage topic
+    this.rosService.subscribeToRightBatteryVoltage((data: number) => {
+      this.rightBatteryVoltage = data;
+    });
+  }
 }
